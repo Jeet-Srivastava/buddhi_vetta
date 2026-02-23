426df554ea46ede9f080e37643a7dd716d54ce85
shashwat sharma <184537667+portneon@users.noreply.github.com>
2026-02-23 21:56:28 +0530

    Update Page.tsx

--- frontend/src/app/Home/Page.tsx
+++ frontend/src/app/Home/Page.tsx
@@ -48,7 +48,7 @@ export default function HomePage() {
         Type_M: formData.machine_type === 'M'
       };
 
-      const response = await fetch('http://localhost:8000/predict', {
+      const response = await fetch('https://buddhi-vetta.onrender.com/predict', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
